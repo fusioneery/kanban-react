f992dd18980ce55f1a1b688ea223ecd3f700123c
Vlad Abramov <vlados.abramovich@gmail.com>
2019-03-29 18:35:53 +0300

    Updates

---
+++ precache-manifest.9c29701612f5ad36dd56474bddfabea9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab1c73a8ef2bab2e0ff",
+    "url": "/kanban-react/static/js/3.de61901f.chunk.js"
+  },
+  {
+    "revision": "40d81db75936cd4867fe",
+    "url": "/kanban-react/static/css/main.b9cfb9fa.chunk.css"
+  },
+  {
+    "revision": "a76605f30bcb7356d119",
+    "url": "/kanban-react/static/js/runtime~main.682ca97b.js"
+  },
+  {
+    "revision": "e0bde5ec280f7f8e0272",
+    "url": "/kanban-react/static/js/2.87277f61.chunk.js"
+  },
+  {
+    "revision": "0ca705b68f8457891b020dc8e1a5a1bc",
+    "url": "/kanban-react/static/media/done.0ca705b6.svg"
+  },
+  {
+    "revision": "40d81db75936cd4867fe",
+    "url": "/kanban-react/static/js/main.65a43608.chunk.js"
+  },
+  {
+    "revision": "772356bee868c115c18357a41fd33d34",
+    "url": "/kanban-react/static/media/info.772356be.svg"
+  },
+  {
+    "revision": "f58f46ef7ce73f8f680b93ed99df147a",
+    "url": "/kanban-react/static/media/edit.f58f46ef.svg"
+  },
+  {
+    "revision": "a2e39ab4535dab88d667657e4fd90f6a",
+    "url": "/kanban-react/static/media/remove.a2e39ab4.svg"
+  },
+  {
+    "revision": "9ab1c73a8ef2bab2e0ff",
+    "url": "/kanban-react/static/css/3.3a7048e4.chunk.css"
+  },
+  {
+    "revision": "16d16b8caf88226264d15cc55605e2b2",
+    "url": "/kanban-react/index.html"
+  }
+];
